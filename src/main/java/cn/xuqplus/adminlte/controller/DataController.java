ad4073bdab9830762024ed62434d9827dd4cc524
xxj <1479269124@qq.com>
2018-03-20 00:10:26 +0800

    pageable

---
+++ src/main/java/cn/xuqplus/adminlte/controller/DataController.java
@@ -0,0 +1,38 @@
+package cn.xuqplus.adminlte.controller;
+
+import cn.xuqplus.adminlte.domain.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+@RestController
+@RequestMapping("data")
+public class DataController {
+    private static List<User> users;
+
+    static {
+        users = new ArrayList<>();
+        int n = new Random().nextInt(100);
+        for (int i = 0; i++ < n; ) {
+            User user = new User();
+            user.setId(Long.valueOf(i));
+            user.setName("name_" + i);
+            user.setPassword("password_" + i);
+            users.add(user);
+        }
+    }
+
+    @GetMapping("users")
+    public List users(int page, int size) {
+        List result = new ArrayList();
+        for (int i = 0; i <= size; i++) {
+            result.add(users.get(page * size + i));
+        }
+        return result;
+    }
+}
